158e8ac9a737bbc0ea894d351ea1e06fdf419ce6
Noble-nana7 <nnaemekanoble7@gmail.com>
2026-06-18 22:20:41 +0100

    refactor: update product retrieval methods and route to improve clarity and consistency

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,76 +1,25 @@
 const mongoose = require('mongoose');
 const ProductModel = require ('../models/product');
-const { getPagination } = require('../helpers/pagination');
 
-
-
-exports.getAllItems = async (req, res, next) => {
+exports.getAllProducts = async (req, res, next) => {
     try {
-        const { page, limit, skip } = getPagination(req);
+        const products = await ProductModel.find()
+            // .populate('categoryId', 'categoryName description')
+            // .populate('supermarketId', 'businessName email')
+            // .populate('createdBy', 'firstName lastName');
 
-        const totalItems = await InventoryModel.countDocuments();
-
-        const items = await InventoryModel.find()
-            .skip(skip)
-            .limit(limit);
-
-        if (items.length === 0) {
-            return res.status(404).json({
-                message: `Nothing found here. Please upload your products`
-            });
-        }
-
-        const totalPages = Math.ceil(totalItems / limit);
+        console.log(products);
 
         res.status(200).json({
-            message: `Product details fetched successfully`,
-            data: items,
-            pagination: {
-                currentPage: page,
-                perPage: limit,
-                totalItems,
-                totalPages,
-                hasNextPage: page < totalPages,
-                hasPreviousPage: page > 1
-            }
+            message: 'All Products found successfully',
+            data: products
         });
 
     } catch (error) {
         console.log(error);
         next(error);
     }
-};
-
-
-
-
-
-
-
-
-//   formal get all before pagination
-// exports.getAllProducts = async (req, res, next) => {
-//     try {
-//         const products = await ProductModel.find()
-//             // .populate('categoryId', 'categoryName description')
-//             // .populate('supermarketId', 'businessName email')
-//             // .populate('createdBy', 'firstName lastName');
-
-//         console.log(products);
-
-//         res.status(200).json({
-//             message: 'All Products found successfully',
-//             data: products
-//         });
-
-//         next(error);
-//     } catch (error) {
-//         console.log(error);
-//         next(error);
-//     }
-// }
-
-
+}
 
 exports.getOneProduct = async (req, res, next) => {
     try {
